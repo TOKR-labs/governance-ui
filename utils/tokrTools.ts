fa633e83bad5c52d2889a68c28ff54bca16c224e
Alex Smith <alexander.richard.smith@gmail.com>
2022-03-28 13:51:51 -0400

    Fractionalization Features Use Tokrizer Program Directly (#18)
    * Add new instructions
    
    * Actually use Tokrizer program for fractional share features
    
    * Remove test realm ids

--- utils/tokrTools.ts
+++ utils/tokrTools.ts
@@ -92,6 +92,46 @@ export class VaultArgs {
     }],
   ]);
 
+  export class FractionalizeArgs {
+    instruction = 3;
+    number_of_shares: number;
+    constructor(fields: { number_of_shares: number } | undefined = undefined) {
+      if (fields) {
+        this.number_of_shares = fields.number_of_shares;
+      }
+    }
+  }
+  
+  export const FractionalizeSchema = new Map([
+    [FractionalizeArgs, {
+      kind: 'struct',
+      fields: [
+        ['instruction', 'u8'],
+        ['number_of_shares', 'u64'],
+      ]
+    }],
+  ]);
+
+  export class SendFractionArgs {
+    instruction = 4;
+    number_of_shares: number;
+    constructor(fields: { number_of_shares: number } | undefined = undefined) {
+      if (fields) {
+        this.number_of_shares = fields.number_of_shares;
+      }
+    }
+  }
+  
+  export const SendFractionSchema = new Map([
+    [SendFractionArgs, {
+      kind: 'struct',
+      fields: [
+        ['instruction', 'u8'],
+        ['number_of_shares', 'u64'],
+      ]
+    }],
+  ]);
+
 /**
  *
  *
@@ -237,20 +277,18 @@ export async function getVaultInstruction({
       VaultSchema,
       new VaultArgs({ vault_bump: vaultBump, vault_seed: vaultSeed })
     ));
-  
-    console.log("MAX RENT:" + await connection.current.getMinimumBalanceForRentExemption(Vault.MAX_VAULT_SIZE));
-  
-    const vaultAuthority = await Vault.getPDA(vaultKey);
+    
+    const vaultMintAuthority = await Vault.getPDA(vaultKey);
   
     const externalPricingAccountKey = (await PublicKey.findProgramAddress([Buffer.from("external"), vaultKey.toBuffer(), wallet!.publicKey!.toBuffer()], TOKR_PROGRAM))[0]
   
     const fractionMintkey = (await PublicKey.findProgramAddress([Buffer.from("fraction"), vaultKey.toBuffer(), wallet!.publicKey!.toBuffer()], TOKR_PROGRAM))[0]
   
-    const redeemTreasuryKey = (await PublicKey.findProgramAddress([vaultAuthority.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), NATIVE_MINT.toBuffer()], ASSOCIATED_TOKEN_PROGRAM_ID))[0]
+    const redeemTreasuryKey = (await PublicKey.findProgramAddress([vaultMintAuthority.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), NATIVE_MINT.toBuffer()], ASSOCIATED_TOKEN_PROGRAM_ID))[0]
   
-    const fractionTreasuryKey = (await PublicKey.findProgramAddress([vaultAuthority.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), fractionMintkey.toBuffer()], ASSOCIATED_TOKEN_PROGRAM_ID))[0]
+    const fractionTreasuryKey = (await PublicKey.findProgramAddress([vaultMintAuthority.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), fractionMintkey.toBuffer()], ASSOCIATED_TOKEN_PROGRAM_ID))[0]
   
-    // console.log("vaultKey:", vaultKey.toBase58());
+    console.log("Vault Public Key:", vaultKey.toBase58());
     // console.log("vaultAuthority:", vaultAuthority.toBase58());
     // console.log("externalPricingAccountKey:", externalPricingAccountKey.toBase58());
     // console.log("fractionMintkey:", fractionMintkey.toBase58());
@@ -262,8 +300,9 @@ export async function getVaultInstruction({
       {
         keys: [
           { pubkey: wallet!.publicKey!, isSigner: true, isWritable: true },
+          { pubkey: wallet!.publicKey!, isSigner: false, isWritable: true },
           { pubkey: vaultKey, isSigner: false, isWritable: true },
-          { pubkey: vaultAuthority, isSigner: false, isWritable: true },
+          { pubkey: vaultMintAuthority, isSigner: false, isWritable: true },
           { pubkey: externalPricingAccountKey, isSigner: false, isWritable: true },
           { pubkey: fractionMintkey, isSigner: false, isWritable: true },
           { pubkey: redeemTreasuryKey, isSigner: false, isWritable: true },
@@ -313,19 +352,19 @@ export async function getAddTokenInstruction({
 
     const fromAddress = new PublicKey(form.fromAddress)
     const vaultAddress = new PublicKey(form.vaultAddress)
-    const tokenAddress = new PublicKey(form.tokenAddress)
+    const tokenMint = new PublicKey(form.tokenMint)
 
-    const vaultAuthority = await Vault.getPDA(vaultAddress);
-    const safetyDepositBox = await SafetyDepositBox.getPDA(vaultAddress, tokenAddress);
-    const transferAuthorityKey = (await PublicKey.findProgramAddress([Buffer.from("transfer"), vaultAddress.toBuffer(), tokenAddress.toBuffer()], TOKR_PROGRAM))[0]
+    const vaultMintAuthority = await Vault.getPDA(vaultAddress);
+    const safetyDepositBox = await SafetyDepositBox.getPDA(vaultAddress, tokenMint);
+    const transferAuthorityKey = (await PublicKey.findProgramAddress([Buffer.from("transfer"), vaultAddress.toBuffer(), tokenMint.toBuffer()], TOKR_PROGRAM))[0]
   
   
     // const tokenAta = await getAtaPda(w.publicKey, tokenAddress); // todo replace with treasury
-    const tokenStoreKey = (await PublicKey.findProgramAddress([Buffer.from("store"), vaultAddress.toBuffer(), tokenAddress.toBuffer()], TOKR_PROGRAM))[0]
+    const tokenStoreKey = (await PublicKey.findProgramAddress([Buffer.from("store"), vaultAddress.toBuffer(), tokenMint.toBuffer()], TOKR_PROGRAM))[0]
   
     console.log("fromAta: ", fromAddress.toBase58());
     console.log("vault: ", vaultAddress.toBase58());
-    console.log("vaultAuthority: ", vaultAuthority.toBase58());
+    console.log("vaultMintAuthority: ", vaultMintAuthority.toBase58());
     console.log("safetyDepositBox: ", safetyDepositBox.toBase58());
     console.log("transferAuthority: ", transferAuthorityKey.toBase58());
     console.log("tokenStoreKey: ", tokenStoreKey.toBase58());
@@ -338,12 +377,13 @@ export async function getAddTokenInstruction({
     const instruction = new TransactionInstruction(
       {
         keys: [
-          { pubkey: tokenAddress, isSigner: false, isWritable: true },
+          { pubkey: tokenMint, isSigner: false, isWritable: true },
           { pubkey: wallet!.publicKey!, isSigner: true, isWritable: true },
           { pubkey: fromAddress, isSigner: false, isWritable: true },
           { pubkey: transferAuthorityKey, isSigner: false, isWritable: true },
+          { pubkey: wallet!.publicKey!, isSigner: false, isWritable: true },
           { pubkey: vaultAddress, isSigner: false, isWritable: true },
-          { pubkey: vaultAuthority, isSigner: false, isWritable: true },
+          { pubkey: vaultMintAuthority, isSigner: false, isWritable: true },
           { pubkey: tokenStoreKey, isSigner: false, isWritable: true },
           { pubkey: safetyDepositBox, isSigner: false, isWritable: true },
           { pubkey: TOKEN_VAULT_PROGRAM_ID, isSigner: false, isWritable: false },
@@ -389,53 +429,47 @@ export async function getAddTokenInstruction({
   currentAccount: GovernedTokenAccount | undefined
   setFormErrors: any
   }): Promise<UiInstruction> {
+
+  const prerequisiteInstructions: TransactionInstruction[] = []
+
   const isValid =  true; // todo: await validateInstruction({ schema, form, setFormErrors })
 
-
-  const vaultMintAuthority = new PublicKey(form.vaultMintAuthority)
   const vaultAddress = new PublicKey(form.vaultAddress)
-  const tokenStoreAddress = new PublicKey(form.tokenStoreAddress)
-
-  let serializedInstruction = ''
-  const prerequisiteInstructions: TransactionInstruction[] = []
-  
   const vault = await metaplex.programs.vault.Vault.load(connection.current, vaultAddress);
 
-  if (vault.data.state == VaultState.Inactive) {
-    console.log("Vault is inactive, add activate instruction");
-    const txData1 = new metaplex.programs.vault.ActivateVault({feePayer: wallet!.publicKey!},
-      {    
-        vault: vault.pubkey,
-        fractionMint: new PublicKey(vault.data.fractionMint),
-        fractionMintAuthority: vaultMintAuthority,
-        fractionTreasury: new PublicKey(vault.data.fractionTreasury),
-        vaultAuthority: new PublicKey(vault.data.authority),
-        numberOfShares: form.numberOfShares
-      }
-    );
-    txData1.instructions.forEach(x => prerequisiteInstructions.push(x))
-    
+  const fractionMint = await connection.current.getAccountInfo(new PublicKey(vault.data.fractionMint))!;
+
+  const rawMint = MintLayout.decode(fractionMint!.data.slice(0, MintLayout.span));
+  if (!rawMint.mintAuthorityOption) {
+    throw new Error("Incorrect Vault Layout")
   }
+  const vaultMintAuthority = new PublicKey(rawMint.mintAuthority);
 
-  // const safetyDepositBoxes = await vault.getSafetyDepositBoxes(connection);
 
-  const txData2 = new metaplex.programs.vault.MintFractionalShares({feePayer: wallet!.publicKey!},
-    {    
-      vault: vault.pubkey,
-      fractionMint: new PublicKey(vault.data.fractionMint),
-      fractionMintAuthority: vaultMintAuthority,
-      fractionTreasury: new PublicKey(vault.data.fractionTreasury),
-      store: tokenStoreAddress,
-      vaultAuthority: new PublicKey(vault.data.authority),
-      numberOfShares: form.numberOfShares
+  const data = Buffer.from(borsh.serialize(
+    FractionalizeSchema,
+    new FractionalizeArgs({ number_of_shares: form.numberOfShares })
+  ));
+
+  const instruction = new TransactionInstruction(
+    {
+      keys: [
+        { pubkey: wallet!.publicKey!, isSigner: true, isWritable: true },
+        { pubkey: wallet!.publicKey!, isSigner: false, isWritable: true },
+        { pubkey: vaultAddress, isSigner: false, isWritable: true },
+        { pubkey: new PublicKey(vaultMintAuthority), isSigner: false, isWritable: true },
+        { pubkey: new PublicKey(vault.data.fractionMint), isSigner: false, isWritable: true },
+        { pubkey: new PublicKey(vault.data.fractionTreasury), isSigner: false, isWritable: true },
+        { pubkey: TOKEN_VAULT_PROGRAM_ID, isSigner: false, isWritable: false },
+        { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+      ],
+      programId: TOKR_PROGRAM,
+      data: data
     }
   );
-  
-  
-  // serializedInstruction = serializeInstructionToBase64(txData2.instructions)
 
   const obj: UiInstruction = {
-      serializedInstruction,
+      serializedInstruction: serializeInstructionToBase64(instruction),
       isValid,
       governance: currentAccount?.governance,
       prerequisiteInstructions: prerequisiteInstructions,
@@ -470,51 +504,51 @@ export async function getAddTokenInstruction({
 
   const destination = new PublicKey(form.destination)
   const vaultAddress = new PublicKey(form.vaultAddress)
-  const tokenAddress = new PublicKey(form.tokenAddress)
+  const mintAddress = new PublicKey(form.tokenMint)
 
   const prerequisiteInstructions: TransactionInstruction[] = []
 
   const vault = await metaplex.programs.vault.Vault.load(connection.current, vaultAddress);
 
   const destination_ata = await getAtaPda(
-      destination,
-      new PublicKey(vault.data.fractionMint),
+    destination,
+    new PublicKey(vault.data.fractionMint),
   );
-  
+
   console.log("ATA address:", destination_ata.toBase58())
-  
-  const accountInfo = await connection.current.getAccountInfo(destination_ata);
-  
-  if (!accountInfo) {
-    console.log("Creating new ATA for destination");
-    prerequisiteInstructions.push(
-        createAssociatedTokenAccountInstruction(
-          destination_ata,
-          wallet!.publicKey!,
-          destination,
-          new PublicKey(vault.data.fractionMint),
-        ),
-    );
-  
-  }
-  
+
+  const data = Buffer.from(borsh.serialize(
+    SendFractionSchema,
+    new SendFractionArgs({ number_of_shares: form.numberOfShares })
+  ));
 
   const transferAuthorityKey = (await PublicKey.findProgramAddress([Buffer.from("vault"), TOKEN_VAULT_PROGRAM_ID.toBuffer(), vaultAddress.toBuffer()], TOKEN_VAULT_PROGRAM_ID))[0]
-  const tokenStoreKey = (await PublicKey.findProgramAddress([Buffer.from("store"), vaultAddress.toBuffer(), tokenAddress.toBuffer()], TOKR_PROGRAM))[0]
-  
-  console.log("Transfer Authority: ", transferAuthorityKey.toBase58())
-  const withdrawTx = new WithdrawSharesFromTreasury({ feePayer: wallet!.publicKey! }, {
-    store: tokenStoreKey,
-    vault: vaultAddress,
-    destination: destination_ata,
-    fractionTreasury: new PublicKey(vault.data.fractionTreasury),
-    vaultAuthority: wallet!.publicKey!,
-    transferAuthority: transferAuthorityKey,
-    numberOfShares: form.numberOfShares
-  });
+
+  const instruction = new TransactionInstruction(
+    {
+      keys: [
+        { pubkey: mintAddress, isSigner: false, isWritable: true },
+        { pubkey: wallet!.publicKey!, isSigner: true, isWritable: true },
+        { pubkey: destination, isSigner: false, isWritable: true },
+        { pubkey: destination_ata, isSigner: false, isWritable: true },
+        { pubkey: transferAuthorityKey, isSigner: false, isWritable: true },
+        { pubkey: vaultAddress, isSigner: false, isWritable: true },
+        { pubkey: new PublicKey(vault.data.authority), isSigner: false, isWritable: true },
+        { pubkey: new PublicKey(vault.data.fractionMint), isSigner: false, isWritable: true },
+        { pubkey: new PublicKey(vault.data.fractionTreasury), isSigner: false, isWritable: true },
+        { pubkey: TOKEN_VAULT_PROGRAM_ID, isSigner: false, isWritable: false },
+        { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+        { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },
+        { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
+        { pubkey: ASSOCIATED_TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+      ],
+      programId: TOKR_PROGRAM,
+      data
+    }
+  );
 
   const obj: UiInstruction = {
-      serializedInstruction: serializeInstructionToBase64(withdrawTx.instructions[0]), //todo I hope this works
+      serializedInstruction: serializeInstructionToBase64(instruction),
       isValid,
       governance: currentAccount?.governance,
       prerequisiteInstructions: prerequisiteInstructions,
